104e95e1673c5623c67d09b932efbcc42433ca43
ruslan <ruslan.kondratuk46@gmail.com>
2022-11-25 16:29:29 +0200

    start frontend web-shop

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Context } from '..';
+import { useContext } from 'react';
+
+const NavBar = () => {
+    const {user} = useContext(Context);
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default NavBar;
