0a7bd8066a9318581d25e7cffb32c35390b51aeb
ruslan <ruslan.kondratuk46@gmail.com>
2022-11-27 22:50:58 +0200

    start page Auth

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Context } from '..';
 import { useContext } from 'react';
+import { Navbar, Container, Nav} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { SHOP_ROUTE } from '../utils/consts';
+import { observer } from 'mobx-react-lite';
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context);
     return (
-        <div>
-
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Nav.Link style={{color:'white'}} to={SHOP_ROUTE}>Купи девайс</Nav.Link>
+                {user.isAuth ?
+                <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={'outline-light'} className='me-2'>Админ панель</Button>
+                    <Button variant={'outline-light'} >Войти</Button>
+                </Nav>
+                :
+                <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                </Nav> }
+            </Container>
+        </Navbar>
     );
-}
+})
 
 export default NavBar;
--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button, Card, Container, Form, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { REGISTRATION_ROUTE } from '../utils/consts';
 
-class Auth extends Component {
-    render() {
-        return (
-            <div>
-                AUTH
-            </div>
-        );
-    }
+
+const Auth = () => {
+    return (
+            <Container
+                className='d-flex justify-content-center align-items-center'
+                style={{height: window.innerHeight - 54}}
+            >
+                <Card style={{width: 600}} className='p-5'>
+                    <h2 className='m-auto'>Авторизация</h2>
+                    <Form className='d-flex flex-column'>
+                        <Form.Control
+                            className='mt-3'
+                            placeholder='Введите ваш email...'
+                        />
+                        <Form.Control
+                            className='mt-3'
+                            placeholder='Введите ваш пароль...'
+                        />
+                        <Row className='d-flex flex-nowrap justify-content-between mt-3'>
+                            <div style={{width: '60%'}}>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                            </div>
+                            <Button
+                                variant={'outline-success'}
+                                style={{width: 100}}
+                            >
+                                Войти
+                            </Button>
+                        </Row>
+
+                    </Form>
+                </Card>
+
+            </Container>
+    );
 }
 
 export default Auth;
+
